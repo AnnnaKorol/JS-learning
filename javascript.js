549dcdd88a2d81e19a6a68c17c5ddb42d36c4ce0
hak <hak@tutao.de>
2024-09-17 19:00:54 +0200

    add js code

--- javascript.js
+++ javascript.js
@@ -12,8 +12,25 @@ console.log(age);
 
 console.log(23 + 97);
 
-function sport (elem) {
-    return 2+2;
-}
+console.log(10+22+68+96+32+98);
 
-console.log(sport);
+console.log((4+6+9)/77);
+
+
+
+let a = 10;
+
+console.log(a);
+
+console.log(9*a);
+
+let b = 7*2;
+console.log(b);
+
+const max = 57;
+const actual = max - 13;
+const percentage = actual/ max;
+console.log(percentage);
+
+
+
