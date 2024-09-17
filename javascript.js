988095d2482e58e251f09384649d28978fe059ba
hak <hak@tutao.de>
2024-09-17 18:49:03 +0200

    edit js

--- javascript.js
+++ javascript.js
@@ -2,13 +2,13 @@ let name = "John";
 let surname = "Doe";
 
 console.log(name);
-console.log("surname");
+console.log(surname);
 
 let age = 11;
 console.log(age);
 
 age = 54;
-console.log("age");
+console.log(age);
 
 console.log(23 + 97);
 
@@ -16,3 +16,4 @@ function sport (elem) {
     return 2+2;
 }
 
+console.log(sport);
