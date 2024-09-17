ac232be122f488db89a2f77db3033c8b83c5cea5
hak <hak@tutao.de>
2024-09-17 18:46:21 +0200

    add js and html

---
+++ javascript.js
@@ -0,0 +1,18 @@
+let name = "John";
+let surname = "Doe";
+
+console.log(name);
+console.log("surname");
+
+let age = 11;
+console.log(age);
+
+age = 54;
+console.log("age");
+
+console.log(23 + 97);
+
+function sport (elem) {
+    return 2+2;
+}
+
